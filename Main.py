d213b7576ee2b2475225acd44f6f4b577fef2b62
URK21CS1178 <110010623+URK21CS1178@users.noreply.github.com>
2022-07-26 11:04:13 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,21 +4,42 @@ class Stack:
         self.size = size
 
     def is_empty(self):
-        # Write code here
+         if self.top == -1 :
+     return 1
+    else :
+     return 0
 
     def is_full(self):
-        # Write code here
-
+        if self.top == (self.size - 1) :
+     return 1
+   else :
+     return 0
+   
     def push(self, data):
         if not self.is_full():
-            # Write code here
+             print("The stack is full")
+          return "Push is not allowed"
+      else:
+          self.top = self.top+1
+          self.l[self.top]=y
 
     def pop(self):
         if not self.is_empty():
-            # Write code here
+             if self.isEmpty()==1:
+          return "the stack is empty"
+      else:
+          print(self.l[self.top])
+          self.top = self.top-1
+          return "Array removed"
 
     def status(self):
-        # Write code here
+              for x in range(self.top+1):
+          print(self.l[x])
+
+size = int(input("Enter the size of the stack:"))
+
+st1 = stackADT(size)
+
 
 # Do not change the following code
 size, queries = map(int, input().rstrip().split())
