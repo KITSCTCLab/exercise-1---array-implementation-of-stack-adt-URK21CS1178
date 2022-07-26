4a7793fcb427a89494e0dac4ede4e92a6024e6a9
URK21CS1178 <110010623+URK21CS1178@users.noreply.github.com>
2022-07-26 14:31:30 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,45 +1,42 @@
 class Stack:
-   def __init__(self,s):
-    self.size = s
-    self.lst = [None]*s
-    self.top = -1
-   def isFull(self) :
-     if self.top == (self.size - 1) :
-        return 1
-     else :
-        return 0
-   def isEmpty(self) :
-     if self.top == -1 :
-        return 1
-     else :
-        return 0
-   def push(self,a):
-     if self.isFull() !=1:
-        self.top+=1
-        self.lst[self.top]=a
-        print("Element Sucessfully Added to the stack.")
-     else:
-        print("Stack is Full.")
-        print("Cannot Push an element..")
-   def pop(self):
-     if self.isEmpty()!=1:
-        t=self.lst[self.top]
-        del self.lst[self.top]
-        self.top-=1
-        return t
-     else:
-        return "Stack is Empty\nCannot Pop an element..."
-   def peek(self):
-     if self.isEmpty()!=1:
-        return self.lst[self.top]
-     else:
-       print("Stack is Empty.")
-   def display(self):
-     if self.isEmpty()!=1:
-        for i in range(0,self.top+1):
-           print(self.lst[i])
-    
-       
+    def __init__(self, size):
+       self.size = size
+       self.lst = [None]*size
+       self.top = -1 
+
+    def is_empty(self):
+        # Write code here
+        if self.top == -1 :
+            return 1
+        else :
+            return 0
+
+    def is_full(self):
+        # Write code here
+        if self.top == (self.size - 1) :
+            return 1
+        else :
+            return 0
+
+    def push(self, data):
+        if not self.is_full():
+            # Write code here
+            self.top+=1
+            self.lst[self.top]=data
+
+    def pop(self):
+        if not self.is_empty():
+            # Write code here
+            del self.lst[self.top]
+            self.top-=1
+            
+
+    def status(self):
+        # Write code here
+         if not self.is_empty():
+            for i in range(0,self.top+1):
+                print(self.lst[i])
+
 # Do not change the following code
 size, queries = map(int, input().rstrip().split())
 stack = Stack(size)
@@ -49,4 +46,4 @@ for line in range(queries):
         stack.push(values[1])
     elif values[0] == 2:
         stack.pop()
-Stack.status()
+stack.status()
